146c9277497f0aa7738c11d45568b249bd469c84
Hari Krishna Battula <142043674+harikrishnaaa321@users.noreply.github.com>
2025-01-01 12:54:53 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		// origin: ["http://localhost:3000"],
-		origin: ["https://hey-chat-backend-mu.vercel.app/"],
+		origin: ["https://hey-chat-backend-mu.vercel.app"],
 		methods: ["GET", "POST"],
 	},
 });
